69d31b9134c9c65022ad406f71969851853f447b
Vijoy Sasidharan <mail@cybernetics.me>
2024-09-01 17:16:28 +0400

    small updates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -87,7 +87,7 @@
                     </div>
                     <div class="col-lg-6">
                         <div class="captcha mt-3">
-                            <span>{!! captcha_img('math') !!}</span>
+                            <span>{!! captcha_img('flat') !!}</span>
                             <a class="btn btn-success btn-refresh"><i class="fa fa-refresh"></i></a>
                         </div>
                     </div>
